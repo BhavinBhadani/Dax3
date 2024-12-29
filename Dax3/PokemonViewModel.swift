ca67d1a2c4d7def98cd66d2d537f5d99eb900905
Bhavin B <bhavinbhadani@yahoo.in>
2024-12-29 11:51:36 +0530

    setup view model

---
+++ Dax3/PokemonViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  PokemonViewModel.swift
+//  Dax3
+//
+//  Created by Bhavin Bhadani on 29/12/24.
+//
+
+import Foundation
+
+@MainActor
+class PokemonViewModel: ObservableObject {
+    enum Status {
+        case notStarted
+        case fetching
+        case success
+        case failed(error: Error)
+    }
+    
+    @Published private(set) var status = Status.notStarted
+    
+    private let contoller: FetchController
+    
+    init(contoller: FetchController) {
+        self.contoller = contoller
+        
+        Task {
+            await getPokemon()
+        }
+    }
+    
+    private func getPokemon() async {
+        status = .fetching
+        
+        do {
+            var pokedex = try await contoller.fetchAllPokemon()
+            pokedex.sort { $0.id < $1.id }
+            
+            for pokemon in pokedex {
+                let newPokemon = Pokemon(context: PersistenceController.shared.container.viewContext)
+                newPokemon.id = Int16(pokemon.id)
+                newPokemon.name = pokemon.name
+                newPokemon.types = pokemon.types
+                newPokemon.hp = Int16(pokemon.hp)
+                newPokemon.attack = Int16(pokemon.attack)
+                newPokemon.defense = Int16(pokemon.defense)
+                newPokemon.specialAttack = Int16(pokemon.specialAttack)
+                newPokemon.specialDefense = Int16(pokemon.specialDefense)
+                newPokemon.speed = Int16(pokemon.speed)
+                newPokemon.sprite = pokemon.sprite
+                newPokemon.shiny = pokemon.shiny
+                newPokemon.favorite = false
+                
+                try PersistenceController.shared.container.viewContext.save()
+            }
+            
+            status = .success
+        } catch {
+            status = .failed(error: error)
+        }
+    }
+}
