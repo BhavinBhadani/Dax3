eaa6e7e28ac17ce95c37a119cc29fabd777a82cb
Bhavin B <bhavinbhadani@yahoo.in>
2024-12-31 13:33:34 +0530

    Pokemon extension

---
+++ Dax3/Pokemon+Ext.swift
@@ -0,0 +1,50 @@
+//
+//  Pokemon+Ext.swift
+//  Dax3
+//
+//  Created by Bhavin Bhadani on 31/12/24.
+//
+
+import Foundation
+
+extension Pokemon {
+    var background: String {
+        switch self.types![0] {
+        case "normal", "grass", "electic", "poison", "fairy":
+            return "normalgrasselectricpoisonfairy"
+        case "rock", "ground", "steel", "fighting", "ghost", "dark", "psychic":
+            return "rockgroundsteelfightingghostdarkpsychic"
+        case "fire", "dragon":
+            return "firedragon"
+        case "flying", "bug":
+            return "flyingbug"
+        case "ice":
+            return "ice"
+        case "water":
+            return "water"
+        default:
+            return "--"
+        }
+    }
+    
+    var stats: [Stat] {
+        return [
+            Stat(id: 1, label: "HP", value: self.hp),
+            Stat(id: 2, label: "Attack", value: self.attack),
+            Stat(id: 3, label: "Defense", value: self.defense),
+            Stat(id: 4, label: "Special Attack", value: self.specialAttack),
+            Stat(id: 5, label: "Special Defense", value: self.specialDefense),
+            Stat(id: 6, label: "Speed", value: self.speed)
+        ]
+    }
+    
+    var highestStat: Stat {
+        stats.max { $0.value > $1.value }!
+    }
+}
+
+struct Stat: Identifiable {
+    var id: Int
+    var label: String
+    var value: Int16
+}
